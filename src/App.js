fea52382dcc09a30e57ba7d1c60499defa312cc1
Eliana Rivarola <micaelatorre@outlook.es>
2023-07-30 22:59:04 -0300

    add Lazy Loadinh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, { lazy, Suspense } from 'react';
 import { SnackbarProvider } from 'notistack';
 import { SnackbarUtilitiesConfigutator } from './helpers/snackbar-manager';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { PublicRoutes } from './models/routes';
 import { RoutesWithNotFound } from './components/NotFound/RoutesWithNotFound';
 import Home from './pages/Home/Home';
-import Details from './pages/Details/Details';
+
+const Details = lazy(() => import('./pages/Details/Details'));
 
 function App() {
   return (
@@ -13,9 +15,22 @@ function App() {
       <BrowserRouter>
         <RoutesWithNotFound>
           <Route exact path={PublicRoutes.HOME} element={<Home />} />
-          <Route path={`${PublicRoutes.DETAILS}:section/:id/`} element={<Details />} >
-          <Route path={`${PublicRoutes.DETAILS}:section/:id/:name`} element={<Details />} />
-          </Route>
+          <Route
+            path={`${PublicRoutes.DETAILS}:section/:id/`}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Details />
+              </Suspense>
+            }
+          />
+          <Route
+            path={`${PublicRoutes.DETAILS}:section/:id/:name`}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Details />
+              </Suspense>
+            }
+          />
         </RoutesWithNotFound>
       </BrowserRouter>
     </SnackbarProvider>
